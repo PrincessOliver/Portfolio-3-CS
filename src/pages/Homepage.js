4ce3793b6dee796dee3136423385920bbbd0fca1
moaazee <92386051+moaazee@users.noreply.github.com>
2023-12-06 13:26:04 +0100

    homepage display 10 movies

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,23 +2,40 @@ import { NavBar } from "../components/NavBar";
 import React, { useEffect, useState } from 'react';
 
 export const Homepage = () => {
-    const [shownTitles, setshownTitles] = useState(null)
+    const [shownTitles, setShownTitles] = useState([]);
 
     const loggedIn = localStorage.length > 0;
 
     useEffect(() => {
-        getTitles('http://localhost:5001/api/titles')
-    }, [])
+        getTitles('http://localhost:5001/api/titles');
+    }, []);
 
-    const getTitles = async endpoint => {
-        const res = await fetch(endpoint)
-        const json = await res.json()
-    }
+    const getTitles = async (endpoint) => {
+        try {
+            const res = await fetch(endpoint);
+            const json = await res.json();
+
+            if (json && Array.isArray(json.items)) {
+                setShownTitles(json.items);
+            } else {
+                console.error('API did not return an array of items:', json);
+            }
+        } catch (error) {
+            console.error('Error fetching titles:', error);
+        }
+    };
 
     return (
         <>
             <NavBar loggedIn={loggedIn} />
-            {/* {} */}
+            <div>
+                <h2>Movie Titles</h2>
+                <ul>
+                    {shownTitles.map((title) => (
+                        <li key={title.id}>{title.name}</li>
+                    ))}
+                </ul>
+            </div>
         </>
-    )
-}
+    );
+};
