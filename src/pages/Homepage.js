aa2df5af1f59a47764ac9672b9158e9a2f8159d4
Andrei Marius <amha@sitecampaign.com>
2023-12-08 18:43:43 +0100

    first version for movies fetching & pagination

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,12 +3,17 @@ import React, { useEffect, useState } from 'react';
 
 export const Homepage = () => {
     const [shownTitles, setShownTitles] = useState([]);
+    const [page, setPage] = useState(0);
 
     const loggedIn = localStorage.length > 0;
 
     useEffect(() => {
-        getTitles('http://localhost:5001/api/titles');
+        getTitles('http://localhost:5001/api/titles/movies');
     }, []);
+
+    useEffect(() => {
+        getTitles(`http://localhost:5001/api/titles/movies?page=${page}&pageSize=10`)
+    }, [page])
 
     const getTitles = async (endpoint) => {
         try {
@@ -29,12 +34,24 @@ export const Homepage = () => {
         <>
             <NavBar loggedIn={loggedIn} />
             <div>
-                <h2>Movie Titles</h2>
-                <ul>
-                    {shownTitles.map((title) => (
-                        <li key={title.id}>{title.name}</li>
-                    ))}
-                </ul>
+                {page > 0 && <button onClick={() => { if (page > 0) { setPage(page -1) }}}>Prev</button>}
+                <button onClick={() => setPage(page + 1)}>Next</button>
+                <div className="container mt-4">
+                    <h2>Movie Titles</h2>
+                    <div className="row">
+                        {shownTitles.map((title, index) => (
+                            <div className="col-md-4 mb-4" key={index}>
+                                <div className="card">
+                                    <img className="card-img-top" src={title.poster} alt="poster" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{title.name}</h5>
+                                        <p className="card-text">{Math.round(title.weightAvgRating * 10) / 10}/10</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </>
     );
