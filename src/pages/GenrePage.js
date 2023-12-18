a82081d4a6bf41839464c86a5957ad135f0b8383
moaazee <92386051+moaazee@users.noreply.github.com>
2023-12-18 12:08:12 +0100

    Style on the genrepage

--- src/pages/GenrePage.js
+++ src/pages/GenrePage.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
+import noPoster from '../Information_Missing_Mock_MC_Patch.jpg';
 
 const GenrePage = () => {
   const { genre } = useParams();
   const [titles, setTitles] = useState([]);
   const [page, setPage] = useState(0);
-  const location = useLocation()
+  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchTitlesByGenre(`http://localhost:5001/api/titles/genre/${genre}`);
   }, [location]);
 
   useEffect(() => {
-    if (page > 0) fetchTitlesByGenre(`http://localhost:5001/api/titles/genre/${genre}?page=${page - 1}&pageSize=10`)
-  }, [page])
+    if (page > 0) {
+      fetchTitlesByGenre(`http://localhost:5001/api/titles/genre/${genre}?page=${page - 1}&pageSize=10`);
+    }
+  }, [page]);
 
-  const fetchTitlesByGenre = async endpoint => {
+  const fetchTitlesByGenre = async (endpoint) => {
     try {
       const response = await fetch(endpoint);
       const data = await response.json();
@@ -29,27 +33,46 @@ const GenrePage = () => {
 
   return (
     <>
-        <ReactPaginate
-            breakLabel="..."
-            nextLabel="next"
-            // pageRangeDisplayed={5}
-            previousLabel="previous"
-            renderOnZeroPageCount={null}
-            pageCount={titles.numberOfPages}
-            onPageChange={(e) => setPage(e.selected + 1)}
-            marginPagesDisplayed={3}
-            containerClassName='pagination'
-            activeClassName='pagination-active'
-        />
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next"
+        previousLabel="previous"
+        renderOnZeroPageCount={null}
+        pageCount={titles.numberOfPages}
+        onPageChange={(e) => setPage(e.selected + 1)}
+        marginPagesDisplayed={3}
+        containerClassName='pagination'
+        activeClassName='pagination-active'
+      />
+      <div className="container mt-4">
         <h2>{genre} Titles</h2>
-        {titles.items?.map((title, index) => (
-          <ul key={index}>
-            <li>{title.name}</li>
-            <li>{title.weightAvgRating}</li>
-            <img src={title.poster} alt='poster' />
-          </ul>
-        ))}
+        <div className="row">
+          {titles.items?.map((title, index) => (
+            <GenreTitleCard key={index} title={title} />
+          ))}
+        </div>
+      </div>
     </>
+  );
+};
+
+const GenreTitleCard = ({ title }) => {
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate(`/title/${title.id}`);
+  };
+
+  return (
+    <div className="col-md-4 mb-4">
+      <div className="card" onClick={handleCardClick} style={{ cursor: 'pointer' }}>
+        <img className="card-img-top" src={title.poster !== 'N/A' ? title.poster : noPoster} alt="poster" />
+        <div className="card-body">
+          <h5 className="card-title">{title.name}</h5>
+          <p className="card-text">{title.weightAvgRating}</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
